51863bee71762838cf1802d75f4b0eb42dd63e5a
BA Demba~ <badembajob@gmail.com>
2022-05-03 02:24:00 +0200

    1er ajout

---
+++ code/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.scenario {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.scenario to javafx.fxml;
+    exports com.example.scenario;
+    exports vue;
+    exports modele;
+
+}
