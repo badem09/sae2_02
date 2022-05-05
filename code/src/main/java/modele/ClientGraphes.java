ccdebae6c753482bea73d3a8ddd95ecd34129a36
BA Demba~ <badembajob@gmail.com>
2022-05-06 00:34:46 +0200

    Ajout client classe Graphes

---
+++ code/src/main/java/modele/ClientGraphes.java
@@ -0,0 +1,19 @@
+package modele;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClientGraphes {
+    public static void main(String[] args) throws IOException {
+
+        Scenario s = Scenario.lectureScenario(new File(
+                "/Users/badem/Documents/INFI1/SAE/algos2/Documentation/scenario_1_1.txt"));
+        Graphes g = new Graphes(s);
+       // System.out.println(s.membresToVilles(s.getDicoAV()));
+        System.out.println(g.setSource());
+        System.out.println(g.getListeAdj());
+
+        System.out.println(s.getMembreScenario());
+
+    }
+}
