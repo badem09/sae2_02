0a8f42db9eedb9b5f0085d79b385059a10d46258
BA Demba~ <badembajob@gmail.com>
2022-05-06 23:55:34 +0200

    1er ajout client classe TempsItineraire

--- code/src/main/java/modele/ClientTempsItineraire.java
+++ code/src/main/java/modele/ClientTempsItineraire.java
@@ -8,13 +8,19 @@ public class ClientTempsItineraire {
     public static void main(String [] arg) throws IOException {
         Scenario s = Scenario.lectureScenario(new File(
                 "/Users/badem/Documents/INFI1/SAE/algos2/Documentation/scenario_1_1.txt"));
-        Itineraire it = new Itineraire(s);
 
+        Itineraire it = new Itineraire(s);
+        System.out.println(it.listeAdj);
         TempsItineraire ti = new TempsItineraire(it);
-        //System.out.println(ti.getDicoItineraire());
-        ArrayList<String> best = ti.getBestItinéraire();
-        int time = ti.getDicoItineraire().get(ti.getBestItinéraire());
-       // System.out.println(TempsItineraire.membresToVilles(best).toString() + " " + time);
-        System.out.println(ti.toString());
+        //System.out.println(ti.getDic: oItineraire());
+        //ArrayList<String> best = ti.getBestItinéraire();
+        System.out.println(ti.toString() + "\n");
+        System.out.println(ti.getBestItineraire());
     }
 }
+/*
+[Pr�sidentD�but, Soporifik, Flagadossnote, N�nupiot, Spoink, Caratroc, Excelangue, Maskadra, Drattaknote, Nidoking, Drackhaus, Pr�sidentFin]
+[Velizy, Amiens, Angers, Biarritz, Bordeaux, Brest, Clermond_Fd, Calais, Cherbourg, Grenoble, Dijon, Velizy]
+longueur : 5209
+
+*/
