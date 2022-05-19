8b25c83b1042dd494159968e5d4069e590284c4e
BA Demba <badembajob@gmail.com>
2022-05-20 00:01:16 +0200

    Suppression du champs mapAjd et remplacement en 2 nouveaux chanmps | suppression de getSource() devenue inutile

--- code/src/main/java/modele/ClientGraphes.java
+++ code/src/main/java/modele/ClientGraphes.java
@@ -7,13 +7,13 @@ public class ClientGraphes {
     public static void main(String[] args) throws IOException {
 
         Scenario s = Scenario.lectureScenario(new File(
-                "/Users/badem/Documents/INFI1/SAE/algos2/Data/scenario_1_1.txt"));
+                "src/main/resources/scenario_0.txt"));
         Graphes g = new Graphes(s);
        // System.out.println(s.membresToVilles(s.getDicoAV()));
-        System.out.println(g.setSource());
-        System.out.println(g.getMapAjd());
+       // System.out.println(g.getSource());
+        System.out.println(g.getMapAjdEntrant());
 
-        System.out.println(s.getMembreScenario());
+       // System.out.println(s.getMembreScenario());
 
     }
 }
