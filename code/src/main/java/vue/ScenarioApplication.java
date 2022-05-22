9f68a6cf054492c159d0eaac1f51b247ce30843d
BA Demba <badembajob@gmail.com>
2022-05-22 19:05:16 +0200

    Ajout page d'acceuil

--- code/src/main/java/vue/ScenarioApplication.java
+++ code/src/main/java/vue/ScenarioApplication.java
@@ -1,32 +1,41 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import modele.Scenario;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ScenarioApplication extends Application {
+    private Scene currentScene;
     @Override
-    public void start(Stage stage) throws Exception {
-        VBox root = new VBox(10);
-        String path = "/Users/badem/Documents/INFI1/SAE/algos2/Documentation/scenario_0.txt";
+    public void start(Stage stage) throws IOException {
+        VBox root = new VBox();
         Scenario s1 = new Scenario();
-        s1 = Scenario.lectureScenario(new File(path));
-        System.out.println(s1);
-        Label labelScenario = new Label (s1.toString());
-        root.getChildren().add(labelScenario);
-
-        //root.getChildren().add(labelHelloBis);
-        Scene scene = new Scene(root,300,80);
-        stage.setScene(scene);
+        Scene sceneAcceuil = new Scene(root,500,370);
+        root.getChildren().addAll(new VBoxAcceuil());
+        stage.setTitle("APLI");
+        stage.setScene(sceneAcceuil);
         stage.show();
+        sceneAcceuil.setUserData("acceuil");
+        this.currentScene = sceneAcceuil;
     }
 
     public static void main(String[] args) {
         Application.launch(args);
     }
+
+
 }
 
