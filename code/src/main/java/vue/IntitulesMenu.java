f0c7e62a85bea7bd16ede240488f28ee3ce0b5f4
BA Demba <badembajob@gmail.com>
2022-05-23 23:19:34 +0200

    Ajout catégorie Itineraire et des ses sous-menu"

--- code/src/main/java/vue/IntitulesMenu.java
+++ code/src/main/java/vue/IntitulesMenu.java
@@ -2,8 +2,10 @@ package vue;
 
 public interface IntitulesMenu {
 
-    String[] ITEM_MENU  = {"Scénarios "};
-    String[] SOUS_MENU = {"Scenario 0","Scenario 1.1" , "Scenario 1.2", "Scenario 2.1",
-            "Scenario 2.2 (Optionnel)"}
-    ;
+    String[] ITEM_MENU  = {"Scénarios", "Itinéraires"};
+    String[][] SOUS_MENU = {{"Scenario 0","Scenario 1.1" , "Scenario 1.2", "Scenario 2.1",
+            "Scenario 2.2 (Optionnel)"},
+            {"Tous les itinéraires", "Votre itinéraire"}
+    };
 }
+
