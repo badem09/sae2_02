642a71a08e4dec7e692f8688e7f570c01276456b
BA Demba <badembajob@gmail.com>
2022-05-26 06:02:46 +0200

    Detail des menus et string des Menus seconds (qui envoient vers d'autres stages

--- code/src/main/java/vue/IntitulesMenu.java
+++ code/src/main/java/vue/IntitulesMenu.java
@@ -3,9 +3,10 @@ package vue;
 public interface IntitulesMenu {
 
     String[] ITEM_MENU  = {"Scénarios", "Itinéraires"};
-    String[][] SOUS_MENU = {{"Scenario 0","Scenario 1.1" , "Scenario 1.2", "Scenario 2.1",
+  /*  String[][] SOUS_MENU = {{"Scenario 0","Scenario 1.1" , "Scenario 1.2", "Scenario 2.1",
             "Scenario 2.2 (Optionnel)"},
             {"Tous les itinéraires", "Votre itinéraire"}
-    };
+    };*/
+    String[][] SOUS_MENU = {{"Ajout d'un scénario","Scénario connus"},{"Tous les itinéraires","Votre itinéraire"}};
 }
 
