3b84ed2b01cf04d7a5b8fb2e6bec11bb9ba3769a
BA Demba <badembajob@gmail.com>
2022-05-26 23:26:10 +0200

    Passage des variables locales en champs et  écriture accecceurs

--- code/src/main/java/vue/StageScenarioConnu.java
+++ code/src/main/java/vue/StageScenarioConnu.java
@@ -5,23 +5,37 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import modele.Scenario;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class StageScenarioConnu extends Stage {
+    private VBoxMenu vBoxMenu;
+    private GridPaneOrg gridPaneOrg;
+
+
+
 
     public StageScenarioConnu() throws IOException {
+
+
         VBox vBox = new VBox();
-        VBoxMenu vBoxMenu = new VBoxMenu();
+        vBoxMenu = new VBoxMenu();
         ComboBox<String> comboScenario = new ComboBox<>();
-        GridPaneOrg gridPaneOrg = new GridPaneOrg();
+        gridPaneOrg = new GridPaneOrg();
         vBox.getChildren().addAll(vBoxMenu, gridPaneOrg);
         Scene scene = new Scene(vBox, 900, 550);
         this.setScene(scene);
         this.getIcons().add(new Image(new FileInputStream(
                 "src/main/resources/pokeman.png")));
-
     }
 
+    public GridPaneOrg getGridPaneOrg() {
+        return gridPaneOrg;
+    }
+
+    public VBoxMenu getvBoxMenu() {
+        return vBoxMenu;
+    }
 }
