90d368095f4154354ba54c569ec8fd4d1130d32b
BA Demba <badembajob@gmail.com>
2022-05-27 20:00:59 +0200

    Ajout du CSS / j'abandonne les Stages / Accesseur

--- code/src/main/java/vue/PageMain.java
+++ code/src/main/java/vue/PageMain.java
@@ -3,19 +3,34 @@ package vue;
 import Controleur.ControleurMenu;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class PageMain extends Application {
 
-    private static StageScenarioConnu currentStage;
+    private static VBoxRoot root;
+    private ControleurMenu  controleurMenu;
 
-    private static ControleurMenu controleurMenu;
+    public static VBoxScenarioConnu getvBoxScenario() {
+        return root.getVboxScenario();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        currentStage = new StageScenarioConnu();
-        stage = currentStage;
-        controleurMenu = new ControleurMenu(currentStage.getvBoxMenu());
+       // currentStage = new StageScenarioConnu();
+        ///stage = currentStage;
+        root = new VBoxRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        File css = new File("css"+File.separator+"style.css");
+        scene.getStylesheets().add(css.toURI().toString());
+        stage.getIcons().add(new Image(new FileInputStream(
+                "src/main/resources/pokeman.png")));        //controleurMenu = new ControleurMenu(root.getVBoxMenu());
+        stage.setTitle("APLI");
         stage.show();
     }
 
@@ -27,7 +42,8 @@ public class PageMain extends Application {
 
     }
 
-    public static StageScenarioConnu getCurrentStage() {
-        return currentStage;
+    public static VBoxMenu getvBoxMenu() {
+        return root.getVBoxMenu();
     }
+
 }
