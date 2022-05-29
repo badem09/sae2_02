8ab2fc32618930e8de9c9c96bf45370594ceacab
BA Demba <badembajob@gmail.com>
2022-05-29 22:58:26 +0200

    Ajout de VBoxAjoutPreview au StackPane

--- code/src/main/java/vue/VBoxRoot.java
+++ code/src/main/java/vue/VBoxRoot.java
@@ -1,13 +1,16 @@
 package vue;
 
+import Controleur.ControleurMenu;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.font.TextHitInfo;
 import java.io.IOException;
 
 public class VBoxRoot extends VBox {
@@ -17,24 +20,28 @@ public class VBoxRoot extends VBox {
     private static VBoxScenarioConnu vBoxScenario;
     private static VBoxMenu vBoxMenu;
     private static VBoxAllItineraire vBoxAllItineraire;
+    private static VBoxAjoutPreview vBoxAjoutPreview;
+    private static ControleurMenu controleurMenu;
 
 
     public VBoxRoot(Stage stage) throws IOException {
         vBoxMenu = new VBoxMenu(this);
         vBoxScenario = new VBoxScenarioConnu(this);
-        vBoxAjout = new VBoxAjoutScenario(stage);
+        vBoxAjout = new VBoxAjoutScenario(stage,this);
         vBoxAllItineraire = new VBoxAllItineraire();
+        vBoxAjoutPreview = new VBoxAjoutPreview(this);
 
-        Node [] components = new Node[3];
+        Node [] components = new Node[4];
         components[0] = vBoxScenario;
         components[1] = vBoxAjout;
         components[2] = vBoxAllItineraire;
+        components[3] = vBoxAjoutPreview;
 
         System.out.println(components);
         stackPane = new StackPane(components);
         stackPane.getChildren().get(0).toFront();
-     //   stackPane.getChildren().get(0).setVisible(false);
-
+        this.setPadding(new Insets(10));
+        this.setSpacing(10);
         this.getChildren().addAll(vBoxMenu,stackPane);
     }
 
@@ -50,4 +57,7 @@ public class VBoxRoot extends VBox {
         return vBoxScenario.getvBoxMenu();
     }
 
+    public static VBoxAjoutPreview getvBoxAjoutPreview() {
+        return vBoxAjoutPreview;
+    }
 }
