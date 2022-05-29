133e90d9db844b12df249395b28091c559c6fb31
BA Demba <badembajob@gmail.com>
2022-05-29 23:20:28 +0200

    Changement dans l'instanciation des scénarios

--- code/src/main/java/modele/ClientItineraire.java
+++ code/src/main/java/modele/ClientItineraire.java
@@ -6,17 +6,18 @@ import java.util.*;
 
 public class ClientItineraire {
     public static void main(String[] args) throws IOException {
-        Scenario s = Scenario.lectureScenario(new File(
-                "src/main/resources/scenario_1_2.txt"));
+        Scenario s = Scenario.lectureScenario(
+                "src/main/resources/scenario_1_1.txt",true);
         Itineraire it = new Itineraire(s);
+
         //it.get()
 
 
         //TreeMap<String, ArrayList<String>> t = it.mapSourcesSuivantes();
-        System.out.println(it.allItineraireToString());
+       System.out.println(it.allItineraireToString());
         //System.out.println(it.mapAdjSortant);
         //System.out.println(it.mapAdjSortant);
-        Villes ville = new Villes();
+       // Villes ville = new Villes();
         //System.out.println(ville.getMembreToVilles().get("PresidentDebut"));
 
     }
