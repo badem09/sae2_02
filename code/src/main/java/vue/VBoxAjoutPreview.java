fa5c699ca00d3b76ba42ebbb380f9db94000dc80
BA Demba <badembajob@gmail.com>
2022-05-30 11:55:25 +0200

    Ajout d'un bouton Valider et update du combobox listant les scénarios

--- code/src/main/java/vue/VBoxAjoutPreview.java
+++ code/src/main/java/vue/VBoxAjoutPreview.java
@@ -3,11 +3,14 @@ package vue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import modele.Scenario;
 import modele.SuiviScenario;
 
+import java.io.File;
 import java.io.IOException;
 
 public class VBoxAjoutPreview extends VBox {
@@ -16,15 +19,40 @@ public class VBoxAjoutPreview extends VBox {
     private GridPaneOrg gridPaneOrg;
     private ComboBox<String> comboBoxScenario;
     private Scenario scenario;
+    private File selectedFile;
 
     public VBoxAjoutPreview(VBoxRoot root) throws IOException {
         this.setId("opaque");
         gridPaneOrg = new GridPaneOrg();
-        this.getChildren().addAll(gridPaneOrg);
+
+        Button buttonValider = new Button("Valider");
+        buttonValider.setAccessibleText("validerAjoutScenario");
+        buttonValider.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    Scenario s = Scenario.lectureScenario(selectedFile,true);
+                    StackPane stackPane = root.getStackPane();
+                    int last = stackPane.getChildren().size() - 1;
+                    while (!(stackPane.getChildren().get(last) instanceof VBoxScenarioConnu)) {
+                        stackPane.getChildren().get(0).toFront();
+                        root.getVboxScenario().setScenario(s);
+                    }
+                    Scenario.getSuiviScenario().writeSuiviScenario(selectedFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        this.getChildren().addAll(gridPaneOrg,buttonValider);
     }
 
 
     public void setScenario(Scenario scenario) throws IOException {
         gridPaneOrg.setScenario(scenario);
+    }
+    public void setSelectedFile(File selectedFile) {
+        this.selectedFile = selectedFile;
     }
 }
