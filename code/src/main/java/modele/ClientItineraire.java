a8b7bc981a8e6c1ec80b7c7b2e3243fa1303c6cf
BA Demba <badembajob@gmail.com>
2022-06-02 00:09:28 +0200

    Début des tests de la méthode parcoursProgressif

--- code/src/main/java/modele/ClientItineraire.java
+++ code/src/main/java/modele/ClientItineraire.java
@@ -9,12 +9,15 @@ public class ClientItineraire {
         Scenario s = Scenario.lectureScenario(
                 "src/main/resources/scenario_1_1.txt",true);
         Itineraire it = new Itineraire(s);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Soporifik");
+        System.out.println(it.parcoursProgressif("Soporifik", list));
 
         //it.get()
 
 
         //TreeMap<String, ArrayList<String>> t = it.mapSourcesSuivantes();
-       System.out.println(it.allItineraireToString());
+      // System.out.println(it.allItineraireToString());
         //System.out.println(it.mapAdjSortant);
         //System.out.println(it.mapAdjSortant);
        // Villes ville = new Villes();
