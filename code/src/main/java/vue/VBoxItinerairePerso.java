bf8dd32eede2705936e49e307e469ef2bcacd16c
BA Demba <badembajob@gmail.com>
2022-06-15 00:14:52 +0200

    remplacement RadioButton par listView et listeneur pour récuperer la séléction

--- code/src/main/java/vue/VBoxItinerairePerso.java
+++ code/src/main/java/vue/VBoxItinerairePerso.java
@@ -1,19 +1,22 @@
 package vue;
 
+
 import Controleur.ControleurItinerairePerso;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import modele.Itineraire;
-import modele.Scenario;
+import modele.Chemin;
 import modele.SuiviScenario;
 import modele.TempsItineraire;
+import org.controlsfx.control.spreadsheet.Grid;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,21 +25,30 @@ import java.util.Map;
 
 public class VBoxItinerairePerso extends VBox {
 
-  //  private VBoxMenu vBoxMenu;
-
     private ComboBox<String> comboBoxScenario;
-    private ScrollPane scrollPossibilites ;
-
+    private StackPane scrollPossibilites ;
+    private ListView listView;
     private Map<String , TempsItineraire> mapItineraire;
-    private Itineraire currentItineraire ;
     private TextArea textItineraire;
     private TextArea textMembres;
     private ControleurItinerairePerso controleur ;
+    GridPane gridPane;
 
     public VBoxItinerairePerso(VBoxRoot root) throws IOException {
         this.setId("opaque");
         mapItineraire = root.getVboxScenario().getGridPaneOrg().getMapItineraire();
         controleur = new ControleurItinerairePerso(this,mapItineraire);
+
+        listView = new ListView<>();
+        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<CelluleListe>() {
+
+            @Override
+            public void changed(ObservableValue<? extends CelluleListe> observable, CelluleListe oldValue, CelluleListe newValue) {
+                if (listView.getSelectionModel().getSelectedItem() != null){
+                CelluleListe choix = (CelluleListe) listView.getSelectionModel().getSelectedItem();
+                controleur.setCurrentSource(choix.getMembre());}
+            }
+        });
 
         ArrayList<String> liste = SuiviScenario.getListeScenarioSuivi();
         Collections.sort(liste);
@@ -49,7 +61,7 @@ public class VBoxItinerairePerso extends VBox {
         VBox vBoxTitre = new VBox(labelTitre);
         vBoxTitre.setAlignment(Pos.CENTER);
 
-        GridPane gridPane = new GridPane();
+        gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
 
@@ -57,13 +69,22 @@ public class VBoxItinerairePerso extends VBox {
         Label labelMembre = new Label("Les membres rentrés jusqu'ici :");
         Label labelItineraire = new Label("L'itinéraire jusqu'ici :");
 
-        scrollPossibilites = new ScrollPane();
+        scrollPossibilites = new StackPane();
         textItineraire = new TextArea();
         textMembres = new TextArea();
+        textItineraire.setPromptText("[Membre1, Membre2, Membre3, ...]\r" + "Distance : distance");
+        textMembres.setPromptText("Membre 1 : Sa ville\rMembre 2 : Sa ville\rMembre 3 : Sa ville\r...");
+
 
         scrollPossibilites.setPrefHeight(300);
         scrollPossibilites.setPrefWidth(715);
-        scrollPossibilites.setPadding(new Insets(10));
+        scrollPossibilites.setPadding(new Insets(1));
+
+
+        listView.setMinHeight(scrollPossibilites.getPrefHeight());
+        listView.setMaxHeight(scrollPossibilites.getPrefHeight());
+        listView.setMinWidth(scrollPossibilites.getPrefWidth());
+        listView.setMaxWidth(scrollPossibilites.getPrefWidth());
 
         int ligne = 0;
 
@@ -76,14 +97,13 @@ public class VBoxItinerairePerso extends VBox {
         gridPane.add(labelItineraire,0,ligne,1,1);
         gridPane.add(textItineraire,0,ligne+1,2,1);
 
-        Button valider = new Button("Valider");
+        Button valider = new Button("_Valider");
+
+        valider.setMnemonicParsing(true);
         valider.setOnAction(controleur);
+        scrollPossibilites.getChildren().add(listView);
         this.setSpacing(15);
         this.getChildren().addAll(vBoxTitre,comboBoxScenario,gridPane,valider);
-    }
-
-    public ScrollPane getScrollPossibilites() {
-        return scrollPossibilites;
     }
 
     public TextArea getTextItineraire() {
@@ -92,5 +112,13 @@ public class VBoxItinerairePerso extends VBox {
 
     public TextArea getTextMembres() {
         return textMembres;
+    }
+
+    public ListView getListView() {
+        return listView;
+    }
+
+    public void setListView(ListView listView) {
+        this.listView = listView;
     }
 }
