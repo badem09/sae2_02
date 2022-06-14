db918dc9cd44062ed293f864bd76bab0a101567b
BA Demba <badembajob@gmail.com>
2022-06-15 00:21:23 +0200

    Suppr print et ajout documentation méthodes

--- code/src/main/java/modele/SuiviScenario.java
+++ code/src/main/java/modele/SuiviScenario.java
@@ -4,18 +4,25 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Sert à savoir quels scénarios ont été ajoutés a l'application jusqu'ici.
+ */
 public class SuiviScenario {
+
     private static ArrayList<String> listeScenarioSuivi;
 
     public SuiviScenario() throws IOException {
         listeScenarioSuivi = new ArrayList<>();
         RecupereListeSuivi(new File("src/main/resources/suivi_scenarios.txt"));
     }
-    public static boolean RecupereListeSuivi(File fileSuivi) throws IOException {
-        fileSuivi.createNewFile();
-        if (listeScenarioSuivi == null) {
-            listeScenarioSuivi = new ArrayList<>();
-        }
+
+    /**
+     * Lit le fichier suivi_scenarios.txt pour récupérer les scénarios déja enregistrés.
+     * S'il n'existe pas, la liste sera vide.
+     * @param fileSuivi (String) : fichier de suivi.
+     * @throws IOException (FileNotFoundException)
+     */
+    public static void RecupereListeSuivi(File fileSuivi) throws IOException {
         BufferedReader bufferEntree = new BufferedReader(new FileReader(fileSuivi));
         String ligne;
         do {
@@ -25,17 +32,17 @@ public class SuiviScenario {
             }
         }
         while (ligne != null);
-        return listeScenarioSuivi.size() > 0;
     }
 
+    /**
+     * Récupère le fichier de suivi et y écrit le nom du scenario.
+     * @param fileScenario (File) : fichier du scenario à enregistrer.
+     * @throws IOException (FileNotFoundException).
+     */
     public static void writeSuiviScenario(File fileScenario) throws  IOException {
 
         File suiviScenario = new File("src/main/resources/suivi_scenarios.txt");
-        boolean nouveau = suiviScenario.createNewFile();
-        //System.out.println(nouveau);
-
         RecupereListeSuivi(suiviScenario);
-        // System.out.println(suiviScenario);
         if (!listeScenarioSuivi.contains(fileScenario.getName())) {
             BufferedWriter writer = new BufferedWriter(new FileWriter(suiviScenario, true));
             writer.write(fileScenario.getName());
@@ -44,11 +51,8 @@ public class SuiviScenario {
             listeScenarioSuivi.add(fileScenario.getName());
             Collections.sort(listeScenarioSuivi);
         }
-
-   //     System.out.println(listeScenarioSuivi);
     }
     public static ArrayList<String> getListeScenarioSuivi() {
         return listeScenarioSuivi;
     }
-
 }
