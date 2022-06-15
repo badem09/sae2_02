edc0297891ebcd46ac81f095187c2d6636f8a9e1
BA Demba <badembajob@gmail.com>
2022-06-15 21:25:55 +0200

    Implementatio d'une Hbox et ajout d'une imageView pour le pokemon

--- code/src/main/java/vue/CelluleListe.java
+++ code/src/main/java/vue/CelluleListe.java
@@ -1,13 +1,40 @@
 package vue;
 
-public class CelluleListe {
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CelluleListe extends HBox {
 
     private String membre;
    private String infos;
 
-    public CelluleListe(String membre, String infos){
+    public CelluleListe(String membre, String infos) throws FileNotFoundException {
         this.infos = infos;
         this.membre = membre;
+        ImageView imageView;
+        System.out.println(membre);
+        try {
+             imageView = new ImageView(new Image(new FileInputStream(
+                    "src/main/resources/images/" + membre + ".jpg")));
+        }
+        catch (FileNotFoundException fnfe){
+            imageView = new ImageView(new Image(new FileInputStream(
+                    "src/main/resources/images/unknown.jpg")));
+        }
+
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+        HBox vBoxImage = new HBox(imageView);
+        getChildren().addAll(vBoxImage,new Label(membre + " " + infos));
+        setSpacing(10);
+     //   vBoxImage.setAlignment(Pos.CENTER_RIGHT); ne marche pas
     }
 
     @Override
