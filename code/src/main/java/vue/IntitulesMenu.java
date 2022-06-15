95cfa02d9ffaba97f6c27dcd677c9821b9b708af
BA Demba <badembajob@gmail.com>
2022-06-15 17:05:52 +0200

    Suppression d'un Array inutile

--- code/src/main/java/vue/IntitulesMenu.java
+++ code/src/main/java/vue/IntitulesMenu.java
@@ -2,11 +2,9 @@ package vue;
 
 public interface IntitulesMenu {
 
-    String[] ITEM_MENU  = {"_" + "Scénarios",  "_" + "Itinéraires", "_"+"Aide"};
+    String[] ITEM_MENU  = {"_" + "Scénarios",  "_" + "Itinéraires", "_" + "Aide"};
 
-    String[][] SOUS_MENU = {{"_" + "Nouveau Scénario","Scénarios " + "_" +"Enregistrés"}
-            ,{"_" + "Tous les itinéraires","Itinéraire _Personnalisé"}, {}};
-
-    String[][] MNEMONIQUES = {{"N","E"},{"T","P"}};
+    String[][] SOUS_MENU = {{"_" + "Nouveau Scénario", "Scénarios " + "_" + "Enregistrés"}
+            ,{"_" + "Tous les itinéraires", "Itinéraire _Personnalisé"}, {}};
 }
 
