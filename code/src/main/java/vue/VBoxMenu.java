a2321f92538868751af309a65d3f86aa2e2f7071
BA Demba <badembajob@gmail.com>
2022-06-17 17:54:27 +0200

    Ajout du menu 'Aide' et son sous-menu invisible

--- code/src/main/java/vue/VBoxMenu.java
+++ code/src/main/java/vue/VBoxMenu.java
@@ -1,8 +1,11 @@
 package vue;
 
 import Controleur.ControleurMenu;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import modele.*;
 
@@ -34,23 +37,33 @@ public class VBoxMenu extends VBox implements IntitulesMenu {
         chMenuBar = new MenuBar();
         int i = 0;
 
-        for(String intitules : ITEM_MENU) {
+        for(String intitules : MENUS) {
             Menu menu = new Menu(intitules);
+            menu.setUserData(intitules);
             menu.setMnemonicParsing(true);
+            menu.setOnAction(controleurMenu);
             chMenuBar.getMenus().add(menu);
             for(int j = 0; j < SOUS_MENU[i].length; j++){
                 MenuItem menuItem = new MenuItem(SOUS_MENU[i][j]);
                 menuItem.setUserData(SOUS_MENU[i][j]);
                 menuItem.setOnAction(controleurMenu);
-               // menuItem.setAccelerator(KeyCombination.keyCombination(
-                 //"shortcut+" + MNEMONIQUES[i][j]));
-               
                 menuItem.setMnemonicParsing(true);
                 chMenuBar.getMenus().get(i).getItems().add(menuItem);
             }
             i++;
+
+            if (intitules == "_Aide"){
+                System.out.println(intitules);
+                menu.showingProperty().addListener(
+                        (observableValue, oldValue, newValue) -> {
+                            if (newValue) {
+                                // the first menuItem is triggered
+                                menu.getItems().get(0).fire();
+                            }
+                        }
+                );
+            }
         }
-        System.out.println(SuiviScenario.getListeScenarioSuivi());
         this.getChildren().addAll(chMenuBar);
     }
 
