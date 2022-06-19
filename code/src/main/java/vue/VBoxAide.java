ad915c109085e2079b6228aec7d93c103a51f151
BA Demba <badembajob@gmail.com>
2022-06-19 16:45:56 +0200

    Réécriture et espacement du label

--- code/src/main/java/vue/VBoxAide.java
+++ code/src/main/java/vue/VBoxAide.java
@@ -17,28 +17,38 @@ public class VBoxAide extends VBox {
 
     public VBoxAide() throws FileNotFoundException {
         this.setId("opaque");
-        Label label = new Label("Cette page est consacrée a l'assistance, si vous ne comprnenez pas " +
-                "comment marche l'application veuillez trouver ci-dessous des explications.\n"+
-                " 1. Les menus\n" +
-                " 1.1 Scenario enregistré. \n" +
-                " En cliquant sur ce menu , vous aurez le choix du scenario. Après avoir fait votre choix vous aurez a " +
-                "gauche le détail du scénario, a droite les membres impliqués et enfin en dessous l'itinéraire le plus rapide\n"+
-                "Exemple : Le numéro de transaction : le vendeur A vend a l'acheteur B :" +
-                " Ville de départ ----> Ville d'arrivé avec le nombre de km parcourus \n" +
-                "1.2 Nouveau scenario \n" +
-                "En cliquant sur ce menu vous avez la possibilité de créer votre propre scenario a partir d'un fichier " +
-                "au format txt avec la forme Vendeur -> Acheteur qui sera converti en scenario\n " +
-                "1.3 Tous les itinéraires \n" +
-                "Ce menu permet de visualiser tous les itinéraires possibles pour un scénario. C'est a dire qu'après" +
-                " avoir choisi votre scénario vous aurez en visuel toute les possibilités de parcours \n"+
-                "1.4 Itinéraire personnalisé \n" +
-                "Ce menu permet de créer votre propre itinéraire. Tout d'abord vous choisissez un scénario , " +
-                "ensuite vous cliquez le prochain membre que vous souhaitez puis vous validez avec le bouton en" +
-                " bas a gauche de votre écran. \n" +
-                "Quand vous avez fini votre parcours vous avez accès aux membre rentrés ainsi que le chemin et la distance\n"+
-                "1.5 Membres\n"+
-                "Cette rubrique vous permet soit a partir d'une ville de trouver ses membres, soit a partir d'un membre de trouver sa ville."
+        Label label = new Label("Cette page est consacrée a l'assistance, vous pourrez y trouver des explications" +
+                "sur les différentes pages.\n\n"+
+                "     1. Les menus\n\n" +
+                "          1.1 Scenarios enregistrés. \n\n" +
+                "               En cliquant sur ce menu, vous aurez le choix du scenario. Après avoir fait votre choix" +
+                "vous aurez à gauche le détail du scénario, à droite les membres impliqués et enfin en " +
+                "dessous l'itinéraire le plus rapide.\n"+
+
+                "Exemple : Le numéro de transaction : le vendeur A vend a l'acheteur B : " +
+                "Ville de départ ----> Ville d'arrivé avec le nombre de km parcourus.\n\n" +
+
+                "          1.2 Nouveau scenario. \n\n" +
+                "               En cliquant sur ce menu vous avez la possibilité de créer votre propre scenario à partir" +
+                "d'un fichier au format txt avec la forme Vendeur -> Acheteur qui sera converti en scenario.\n\n" +
+
+                "          1.3 Tous les itinéraires. \n\n" +
+                "               Ce menu permet de visualiser tous les itinéraires possibles pour un scénario." +
+                "C'est-à-dire qu'après avoir choisi votre scénario vous aurez en visuel toutes"+
+                "les possibilités de parcours.\n\n" +
+
+                "          1.4 Itinéraire personnalisé. \n\n" +
+                "               Ce menu permet de créer votre propre itinéraire. Tout d'abord vous choisissez un " +
+                "scénario, ensuite vous cliquez le prochain membre que vous souhaitez puis vous " +
+                "validez avec le bouton en bas a gauche de votre écran. \n" +
+                "Quand vous avez fini votre parcours vous avez accès aux membre rentrés ainsi " +
+                "que le chemin et la distance.\n\n"+
+
+                "          1.5 Membres.\n\n"+
+                "               Cette rubrique vous permet soit a partir d'une ville de trouver ses membres, soit à" +
+                "partir d'un membre de trouver sa ville."
         );
+        label.setId("inline");
         label.setWrapText(true);
         this.getChildren().add(label);
     }
